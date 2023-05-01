c60bad6fc225140a469a36628e224305f224ccf8
runner <runner@Mac-1682938598430.local>
2023-05-01 11:36:47 +0000

    KMM SPM package release for 0.1.3

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let remoteKotlinUrl = "https://maven.pkg.github.com/Foso/KMMBridgeKickStart/de/jensklingenberg/kmmtest/allshared-kmmbridge/0.1.2/allshared-kmmbridge-0.1.2.zip"
-let remoteKotlinChecksum = "fe57822fa8ae5806e791558b1a632c3cd3af6f185d8e866ff4e132f40f68a6d4"
+let remoteKotlinUrl = "https://maven.pkg.github.com/Foso/KMMBridgeKickStart/de/jensklingenberg/kmmtest/allshared-kmmbridge/0.1.3/allshared-kmmbridge-0.1.3.zip"
+let remoteKotlinChecksum = "0ab3657558324be5cbcd6a1dfd754458ddde90f125dadb897caf13de70b290c1"
 let packageName = "allshared"
 
 let package = Package(
@@ -15,10 +15,6 @@ let package = Package(
             name: packageName,
             targets: [packageName]
         ),
-        .library(
-            name: "SwiftTest",
-            targets: ["SwiftTest"]
-        )
     ],
     targets: [
         .binaryTarget(
@@ -27,9 +23,5 @@ let package = Package(
             checksum: remoteKotlinChecksum
         )
         ,
-         .target(
-            name: "SwiftTest",
-            path: "SwiftTest"
-        )
     ]
 )
